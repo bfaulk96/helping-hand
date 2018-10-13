5c97003e4273b0358e947fd170ab82ec90d6159e
bfaulkn <bfaulkn@siue.edu>
2018-10-13 08:51:52 -0500

    New logo, who dis? Color matches the hand, because style AF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {Component} from '@angular/core';
 import {Platform} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-root',
@@ -40,7 +41,8 @@ export class AppComponent {
     constructor(
         private platform: Platform,
         private splashScreen: SplashScreen,
-        private statusBar: StatusBar
+        private statusBar: StatusBar,
+        private translate: TranslateService
     ) {
         this.initializeApp();
     }
@@ -49,6 +51,22 @@ export class AppComponent {
         this.platform.ready().then(() => {
             this.statusBar.styleDefault();
             this.splashScreen.hide();
+            this._initTranslate();
         });
+    }
+
+    private _initTranslate(){
+        // Set the default language for translation strings, and the current language.
+        this.translate.setDefaultLang('en');
+
+
+        if (this.translate.getBrowserLang() !== undefined)
+        {
+            this.translate.use(this.translate.getBrowserLang());
+        }
+        else
+        {
+            this.translate.use('en'); // Set your language here
+        }
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,28 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
+}
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+        }
+    }),
     IonicModule.forRoot(),
     AppRoutingModule
   ],
