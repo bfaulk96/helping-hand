032c94199da28c13a1e049df02e4cec8cc2dd495
Alex Nuetzel <karlanuetzel@gmail.com>
2018-10-13 22:35:10 -0500

    picture stuff woohoo yes

--- src/app/vision/vision.page.ts
+++ src/app/vision/vision.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {CameraOptions} from '@ionic-native/camera';
 import {Camera} from '@ionic-native/camera/ngx';
 
+import {DomSanitizer} from '@angular/platform-browser';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
@@ -13,15 +14,18 @@ import {Observable} from 'rxjs';
 })
 export class VisionPage implements OnInit {
     constructor(private camera: Camera,
-                private httpClient: HttpClient) {
+                private httpClient: HttpClient,
+                public _DomSanitizationService: DomSanitizer) {
     }
 
-    public url: string = 'https://vision.googleapis.com/v1/images:annotate?key=YOUR_API';
+    public url: string = "https://vision.googleapis.com/v1/images:annotate?key=YOUR_API";
+    public base64Image: string = "";
 
     public ngOnInit(): void {
+
     }
 
-    public openCamera(): void {
+    public takePicture(): void {
         const options: CameraOptions = {
             quality: 1,
             destinationType: this.camera.DestinationType.DATA_URL,
@@ -31,12 +35,7 @@ export class VisionPage implements OnInit {
 
         this.camera.getPicture(options).then(
             (imageData: string): void => {
-                const base64Image = 'data:image/jpeg;base64,' + imageData;
-                this.sendImageToCloudVisionHandler(base64Image).subscribe((res) => {
-                    console.log(res);
-                }, (err) => {
-                    console.log(err);
-                });
+                this.base64Image = "data:image/jpeg;base64," + imageData;
             },
             (error: Error): void => {
                 console.error(error);
@@ -44,12 +43,20 @@ export class VisionPage implements OnInit {
         );
     }
 
+    sendPicture() {
+        this.sendImageToCloudVisionHandler(this.base64Image).subscribe((res) => {
+            console.log(res);
+        }, (err) => {
+            console.log(err);
+        });
+    }
+
     public sendImageToCloudVisionHandler(content): Observable<any> {
         return this.httpClient.post(this.url, content);
     }
 
-    getImageFromGallery() {
-
+    closePreview() {
+        this.base64Image = "";
     }
 
 
