d8ab2828b764985f7f2d13bb93c4f1629e5c1182
Charles Reusnow <Charles.J.Reusnow@centene.com>
2018-10-14 03:47:06 -0500

    Updated backend

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -1,8 +1,8 @@
 export class Constants {
     public static API: any = {
         ENDPOINTS: {
-            LOCAL: "http://8a3af8c2.ngrok.io",
-            PROD: "http://8a3af8c2.ngrok.io"
+            LOCAL: "http://192.81.219.113:3007",
+            PROD: "http://192.81.219.113:3007"
         },
         VERBS: {
             GET: "GET",
