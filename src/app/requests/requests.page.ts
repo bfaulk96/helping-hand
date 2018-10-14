57f9cd5ed39b3899341414bd6711d2b959a3cabc
Charles Reusnow <Charles.J.Reusnow@centene.com>
2018-10-14 04:26:42 -0500

    token

--- src/app/requests/requests.page.ts
+++ src/app/requests/requests.page.ts
@@ -61,7 +61,7 @@ export class RequestsPage implements OnInit {
                 (token: any): void => {
                     this.httpClient.get(this.apiHelper.getServiceEndPoint() + "/requests/receiver", {
                         headers: {
-                            "Authorization": "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhlbHBlckBoZWxwZXIuY29tIiwiaWF0IjoxNTM5NDk5NTkwLCJleHAiOjE1Mzk1MDMxOTB9.k-VHx-7wWi7hawBT95WToblmAn9btd4RbcPYwn3UvIA"
+                            "Authorization": "Bearer " + token
                         }
                     }).subscribe(
                         (res: Array<Request>) => {
@@ -82,63 +82,75 @@ export class RequestsPage implements OnInit {
 
     public request(helper: User): void {
         const user: User = this.userDAO.getCurrentUser();
-        this.httpClient.post(this.apiHelper.getServiceEndPoint() + "/requests/create", {
-            title: "test title",
-            description: "test description",
-            senderId: user._id,
-            // receiverId: helper._id,
-            receiverId: "5bc2509502ed722737a7100c",
-            helpCategory: user.helpCategories[0]
-        }, {
-            headers: {
-                "Authorization": "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhlbHBlckBoZWxwZXIuY29tIiwiaWF0IjoxNTM5NTA1MjE1LCJleHAiOjE1Mzk1MDg4MTV9.HOgSW3D8H3noRTTq0OeYRuZLfo06bZXnWY9idGbxo6A"
-            }
-        }).subscribe(
-            (res: any): void => {
-                console.log(res);
-            },
-            (error: HttpErrorResponse): void => {
-                console.error(error);
+        this.apiHelper.getAccessToken().subscribe(
+            (token: any): void => {
+                this.httpClient.post(this.apiHelper.getServiceEndPoint() + "/requests/create", {
+                    title: "test title",
+                    description: "test description",
+                    senderId: user._id,
+                    // receiverId: helper._id,
+                    receiverId: "5bc2509502ed722737a7100c",
+                    helpCategory: user.helpCategories[0]
+                }, {
+                    headers: {
+                        "Authorization": "Bearer " + token
+                    }
+                }).subscribe(
+                    (res: any): void => {
+                        console.log(res);
+                    },
+                    (error: HttpErrorResponse): void => {
+                        console.error(error);
+                    }
+                );
             }
         );
     }
 
     public accept(request: Request): void {
-        this.httpClient.post(this.apiHelper.getServiceEndPoint() + "/requests/update-request", {
-            requestId: request._id,
-            isApproved: true,
-            isComplete: request.isComplete,
-            isRejected: false
-        }, {
-            headers: {
-                "Authorization": "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhlbHBlckBoZWxwZXIuY29tIiwiaWF0IjoxNTM5NDk5NTkwLCJleHAiOjE1Mzk1MDMxOTB9.k-VHx-7wWi7hawBT95WToblmAn9btd4RbcPYwn3UvIA"
-            }
-        }).subscribe(
-            (res: any): void => {
-                console.log(res);
-            },
-            (error: HttpErrorResponse): void => {
-                console.error(error);
+        this.apiHelper.getAccessToken().subscribe(
+            (token: any): void => {
+                this.httpClient.post(this.apiHelper.getServiceEndPoint() + "/requests/update-request", {
+                    requestId: request._id,
+                    isApproved: true,
+                    isComplete: request.isComplete,
+                    isRejected: false
+                }, {
+                    headers: {
+                        "Authorization": "Bearer " + token
+                    }
+                }).subscribe(
+                    (res: any): void => {
+                        console.log(res);
+                    },
+                    (error: HttpErrorResponse): void => {
+                        console.error(error);
+                    }
+                );
             }
         );
     }
 
     public reject(request: Request): void {
-        this.httpClient.post(this.apiHelper.getServiceEndPoint() + "/requests/update-request", {
-            requestId: request._id,
-            isApproved: false,
-            isComplete: request.isComplete,
-            isRejected: true
-        }, {
-            headers: {
-                "Authorization": "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhlbHBlckBoZWxwZXIuY29tIiwiaWF0IjoxNTM5NDk5NTkwLCJleHAiOjE1Mzk1MDMxOTB9.k-VHx-7wWi7hawBT95WToblmAn9btd4RbcPYwn3UvIA"
-            }
-        }).subscribe(
-            (res: any): void => {
-                console.log(res);
-            },
-            (error: HttpErrorResponse): void => {
-                console.error(error);
+        this.apiHelper.getAccessToken().subscribe(
+            (token: any): void => {
+                this.httpClient.post(this.apiHelper.getServiceEndPoint() + "/requests/update-request", {
+                    requestId: request._id,
+                    isApproved: false,
+                    isComplete: request.isComplete,
+                    isRejected: true
+                }, {
+                    headers: {
+                        "Authorization": "Bearer " + token
+                    }
+                }).subscribe(
+                    (res: any): void => {
+                        console.log(res);
+                    },
+                    (error: HttpErrorResponse): void => {
+                        console.error(error);
+                    }
+                );
             }
         );
     }
